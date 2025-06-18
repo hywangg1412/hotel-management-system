4be6dd7d933cc1d335f58adc2b9df94afa2bbb5e
huydev <hyperrhy1412@gmail.com>
2025-06-18 09:42:39 +0700

    first commit

---
+++ Objects/Customer.cs
@@ -0,0 +1,27 @@
+﻿namespace Objects
+{
+    public partial class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerFullName { get; set; }
+        public string Telephone { get; set; }
+        public string EmailAddress { get; set; }
+        public DateOnly CustomerDOB { get; set; }
+        public string Status { get; set; }
+        public string Password { get; set; }
+
+        public Customer() { }
+
+        public Customer(int customerId, string customerFullname, string telephone, string emailAddress,
+            DateOnly customerDOB, string status, string password)
+        {
+            CustomerId = customerId;
+            CustomerFullName = customerFullname;
+            Telephone = telephone;
+            EmailAddress = emailAddress;
+            CustomerDOB = customerDOB;
+            Status = status;
+            Password = password;
+        }
+    }
+}
---
+++ Objects/Room.cs
@@ -0,0 +1,28 @@
+﻿namespace Objects
+{
+    public partial class Room
+    {
+        public int RoomId { get; set; }
+        public string RoomNumber { get; set; }
+        public string RoomDescription { get; set; }
+        public int RoomMaxCapacity { get; set; }
+        public string RoomStatus { get; set; }
+        public decimal RoomPricePerDate { get; set; }
+        public int RoomTypeId { get; set; }
+
+        public Room()
+        {
+        }
+
+        public Room(int roomId, string roomNumber, string roomDescription, int roomMaxCapacity, string roomStatus
+            , decimal roomPricePerDate, int roomTypeId)
+        {
+            RoomId = roomId;
+            RoomNumber = roomNumber;
+            RoomDescription = roomDescription;
+            RoomMaxCapacity = roomMaxCapacity;
+            RoomStatus = roomStatus;
+            RoomTypeId = roomTypeId;
+        }
+    }
+}
